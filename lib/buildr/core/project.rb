74b5af1a00b70385e3f949c7b56f607eb527a2be
Ittay Dror <ittay.dror@gmail.com>
2008-12-06 23:22:24 +0200

    avoid creating tasks that don't exist in recursive task

--- lib/buildr/core/project.rb
+++ lib/buildr/core/project.rb
@@ -187,7 +187,8 @@ module Buildr
     module RecursiveTask
       attr_accessor :project, :task_name
       def invoke_prerequisites(*args)
-        @prerequisites |= @project.projects(:immediate => true).map {|project| project.task(@task_name)}
+        @prerequisites |= @project.projects(:immediate => true).
+           map {|project| Buildr.application.lookup(project.name + ':'  + @task_name)}.compact.map(&:to_s)
         super
       end
     end
